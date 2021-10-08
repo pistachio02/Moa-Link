b93537b97a909f1e519ad46358f53ea583ad4240
off-dngw <off-dngw@gmail.com>
2021-10-08 13:17:08 +0900

    Initail Setting 서버 작성 Package.json -> Script내용 수정 했습니다.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
-console.log('Moa Link!@')
+// Moa-Link Server 쪽 Issue 
+// /server -> install -> npm run start X
+// 이유? package.json -> script -> "start index.js" 
+// 누락이 원인
+ 
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+const port = 80;
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
+
+app.get('/', (req, res) => {
+  res.status(201).send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`서버가 ${port}번에서 작동중입니다.`);
+});
