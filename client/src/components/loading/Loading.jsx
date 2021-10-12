b47c926acc3bcbe89ca7ab8a447aaa435ca5606f
leejanghe <babybye777@gmail.com>
2021-10-13 00:35:18 +0900

    loading done and footer fix 로딩페이지 구현 및 푸터 오타 수정하였습니다.

---
+++ client/src/components/loading/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Loading.css'
+
+function Loading() {
+    return (
+    <>
+       <div className="square">
+       <h1 className="loading-text">MoaLink Loading...</h1>
+  <div className="spin"></div>
+        </div>
+    </>
+    )
+}
+
+export default Loading
