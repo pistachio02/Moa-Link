695b7255eba89d139bd3a443e6c5c27d4776bcd5
Lee geon woo <78680884+Geonwoo-Lee@users.noreply.github.com>
2021-10-14 20:12:04 +0900

    진짜 죄송하지만 마지막 입니다

--- client/src/components/urlComponents/Etc.jsx
+++ client/src/components/urlComponents/Etc.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import {Card, UrlComponentWrap, UrlPlusButton } from '../style/Styled'
+import {Card, UrlComponentWrap } from '../style/Styled'
 import * as BiIcons from 'react-icons/bi'
 import axios from 'axios';
-import './Font.css'
-import { Modal } from '../urlCardModalDev/Modal';
 
-function AllUrl() {
+function Etc() {
     const [urlList, setUrlList] =useState([]);
+  
     
     const deleteHandler = async (id) => {
         try {
@@ -15,10 +14,10 @@ function AllUrl() {
             throw error;
         }
     }
-
+  
     const fetchData = async () => {
         try {
-            const response = await axios.get('https://localhost:4000/urlpage')
+            const response = await axios.get('https://localhost:4000/etc')
             const url = response.data.data
             setUrlList(url)
         } catch (error) {
@@ -29,44 +28,33 @@ function AllUrl() {
     useEffect(() =>{
         fetchData()
     },[fetchData])
-    const [isOpen, setIsOpen] = useState(false);
-    const openModalHandler = () => {
-    setIsOpen(!isOpen);
-  };
+  
 
 
     return (
         <div>
-            {isOpen ? 
-<Modal openModalHandler = {openModalHandler}/>
-            :
+            <UrlComponentWrap>
+            {
+            urlList !== undefined &&
 
-<UrlComponentWrap>
-<UrlPlusButton onClick = {openModalHandler}>
-    Url추가 하기
-</UrlPlusButton>
-{
-urlList !== undefined &&
+            urlList.map((el) => (
 
-urlList.map((el) => (
+              <Card>
+              <div className = 'icons'>
+              <BiIcons.BiTrash className = 'trash' key={el.id} onClick = {() => deleteHandler(el.id)}/>
+              </div>
+              <div className = 'title'>{el.title}</div>
+                  <div className = 'des'>{el.description}</div>
+                  <div className = 'url' onClick = {() => window.open(`http://${el.url}`, '_blank')}>Move to Url !</div>
 
-  <Card>
-  <div className = 'icons'>
-  <BiIcons.BiTrash className = 'trash' key={el.id} onClick = {() => deleteHandler(el.id)}/>
-  </div>
-  <div className = 'title'>{el.title}</div>
-      <div className = 'des'>{el.description}</div>
-      <div className = 'url' onClick = {() => window.open(`${el.url}`, '_blank')}>Move to Url !</div>
-
-     </Card>
-    ))}
-
-</UrlComponentWrap>
-
-            }
-           
+                 </Card>
+                ))}
+            
+        </UrlComponentWrap>
+    
+      
   </div>
     )
 }
 
-export default AllUrl
+export default Etc
